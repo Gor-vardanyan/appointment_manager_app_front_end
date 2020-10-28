e7d6250aa54d8d6946d1c1eba718cf9e3ed4d7a2
Gor <g.var.design@gmail.com>
2020-10-28 10:15:58 +0100

    header adjustments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import './App.css';
-//import './Components/Header';
+//import './App.css';
+import './App';
+import {useState, useEffect} from 'react';
+
+import Header from './Components/Header/Header';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import Home from './containers/Home/Home';
+//import Login from './containers/Login/LoginClient';
+//import Register from './containers/Register/Register';
+import axios from 'axios';
 
 function App() {
+console.log(process.env.APP_URL);
+const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+useEffect(() => {
+  const token = localStorage.getItem('authToken')
+  axios.get(process.env.APP_URL + '/users/profile',
+    {
+      headers: {
+        Authorization: token
+      }
+    })
+    .then(res => {
+      setUser(res.data)
+    })
+}, [])
       return (
         <div>
-          Hello 
-          my friend 
+          <Header user={user} setUser={setUser}/>
+            <BrowserRouter>
+              <Switch>
+              </Switch>
+            </BrowserRouter>
         </div>
       );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,3 +2,44 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Header.scss';
 
+// class Header extends React.component{
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             Header
+//         }
+//     }
+// }
+//     render() {
+//         return <div className="user">
+//             <h2 className="name">
+//                 {this.state.user.name}
+//             </h2>
+//         </div>
+//     }
+// }
+const Header = ({user,setUser}) => {
+        const logout = () => {
+            localStorage.clear();
+            // props.setUser(null)
+            setUser(null)
+        }
+    return (
+        <header className="header">
+            <Link to ="/" >Home</Link>
+            {user ?
+                <div className="loggedIn">
+                {['admin','Dios'].includes(user.role) &&<Link to="/users">users</Link> }
+                    <Link to="/profile">{user.email} - {user.role}</Link>
+                    <span className="logout" onClick={logout}>Logout</span>
+                </div> :
+                <div className="notLoggedIn">
+                    <Link to="/client/logInClient">Login</Link>
+                    <Link to="/client/registerClients">Registro</Link>
+                </div>}
+
+        </header>
+    )
+}
+export default Header;
+
